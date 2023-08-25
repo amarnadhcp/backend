325c8a79f4bac2e1c5bd9e556f993a727e5be3ae
Amarnadh <cpamarnadh843@gmail.com>
2023-08-25 14:04:59 +0530

    last changes

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,14 +2,15 @@ import  Express  from "express";
 import {login, users,AddCategory,categorys,userManage,PayFreelancer,Allworks} from "../controllers/admin.controller.js"
 import upload from "../middleware/multer.js";
 const router = Express.Router()
+import { AdminAuth } from "../middleware/AdminAuth.js";
 
 router.post("/login",login)
-router.get("/users",users)
-router.post("/addcategory",upload.single("file"),AddCategory)
-router.get("/categorys",categorys)
-router.post("/usermanage/:id",userManage)
-router.post("/payment",PayFreelancer)
-router.get("/works",Allworks)
+router.get("/users",AdminAuth,users)
+router.post("/addcategory",AdminAuth,upload.single("file"),AddCategory)
+router.get("/categorys",AdminAuth,categorys)
+router.post("/usermanage/:id",AdminAuth,userManage)
+router.post("/payment",AdminAuth,PayFreelancer)
+router.get("/works",AdminAuth,Allworks)
 
 
 
